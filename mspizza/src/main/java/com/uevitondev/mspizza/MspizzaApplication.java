ba8c52e3b1db58dbf6b46be325fa9756f1e86dd6
uevitondev <uevitonsantos@outlook.com>
2023-07-31 09:40:36 -0300

    project mspizza created

---
+++ mspizza/src/main/java/com/uevitondev/mspizza/MspizzaApplication.java
@@ -0,0 +1,13 @@
+package com.uevitondev.mspizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MspizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MspizzaApplication.class, args);
+	}
+
+}
---
+++ mspizza/src/test/java/com/uevitondev/mspizza/MspizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uevitondev.mspizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MspizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
